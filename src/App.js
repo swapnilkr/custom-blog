9f547355c2dd6b106ecb2502a8994d31ef327f22
swapnilkr099 <swapnilkr099@gmail.com>
2024-06-26 02:33:34 +0530

    Integrated footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <Header>
-      
-    </Header>
-  );
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+
+	);
 }
 
 export default App;
