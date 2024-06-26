918f5462c772e8fd88130367fb0420bfd76185fc
swapnilkr099 <swapnilkr099@gmail.com>
2024-06-26 20:56:12 +0530

    Made banner image responsive

--- src/components/BannerImage.js
+++ src/components/BannerImage.js
@@ -1,12 +1,34 @@
-import '../styles/BannerImage.css'
-import HeaderImg from "../assets/header.jpg"
+import { useEffect, useState } from 'react';
+import '../styles/BannerImage.css';
+import HeaderImgZoom from '../assets/header_zoom.jpg'
+import HeaderImgLowRes from '../assets/header.jpg'
 
 const BannerImage = () => {
+    const [isLowRes, setIsLowRes] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsLowRes(window.innerWidth <= 600);
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <div className="banner">
+        <div className={`banner`}>
+            {isLowRes ?
+                <img src={HeaderImgLowRes} alt="Banner" />
+                :
+                <img src={HeaderImgZoom} alt="Banner" />
+            }
             <p className='banner-text'>The Truecaller Blog</p>
         </div>
-    )
+    );
 }
 
-export default BannerImage
+export default BannerImage;
