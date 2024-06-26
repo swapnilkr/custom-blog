1090da76579c5dad960b1d64c5fcec8d0a1544d2
swapnilkr099 <swapnilkr099@gmail.com>
2024-06-26 09:44:36 +0530

    Added Banner Image

---
+++ src/components/BannerImage.js
@@ -0,0 +1,12 @@
+import '../styles/BannerImage.css'
+import HeaderImg from "../assets/header.jpg"
+
+const BannerImage = () => {
+    return (
+        <div className="banner">
+            <p className='banner-text'>The Truecaller Blog</p>
+        </div>
+    )
+}
+
+export default BannerImage
